ff2f6c3b6bddb5378fe9228a8e8afda7808a0561
admin <admin@admins-MacBook-Air.local>
2026-07-13 20:25:48 -0600

    Revert "Fix repeated builder placement taps"
    This reverts commit 0f43fb576a4ffd3d7732a649616520c6061b6516.

--- unity/LTW.UnityClient/Assets/Scripts/UI/TouchPlacementController.cs
+++ unity/LTW.UnityClient/Assets/Scripts/UI/TouchPlacementController.cs
@@ -313,13 +313,13 @@ namespace LTW.UnityClient.UI
             if (result.Accepted)
             {
                 feedbackView.ShowAccepted(SelectedTowerName() + " placed");
+                placementPreview = result;
+                UpdateGhostColor();
                 if (!stayInPlacementMode)
                 {
                     CancelPlacement(false);
-                    return true;
                 }
 
-                RefreshPlacementPreview();
                 return true;
             }
 
@@ -848,6 +848,11 @@ namespace LTW.UnityClient.UI
             }
 
             if (isPaletteExpanded && TowerPalettePanelRect(scale, frame).Contains(guiPoint))
+            {
+                return true;
+            }
+
+            if (isPlacing && PlacementPanelRect(scale, frame).Contains(guiPoint))
             {
                 return true;
             }
